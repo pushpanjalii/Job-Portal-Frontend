4ece21f313d48aa5e8ddd8415a4dba2f0ec39a5a
pushpanjali <vermapushpanjali5@gmail.com>
2024-11-18 21:25:08 +0530

    MONGO cluster generated

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 import express from "express";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import dotenv from "dotenv";
+dotenv.config({});
 
 const app = express();
+
+// app.get("/home", (req,res) => {
+//     return res.status(200).json({
+//         message: "coming form backend",
+//         success:true
+//     })
+// });
 
 //middleware
 app.use(express.json());
@@ -14,8 +23,8 @@ const corsOptions = {
 };
 app.use(cors(corsOptions));
 
-const port = 3000;
-app.listen(port,() => {
-    console.log("App is listening on port 3000");
+const PORT = process.env.PORT || 3000
+app.listen(PORT,() => {
+    console.log(`App is listening on port ${PORT}`);
 })
 
