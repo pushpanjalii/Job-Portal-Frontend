729f9c57046dc451dd56ec58062d07f7081e85a3
pushpanjali <vermapushpanjali5@gmail.com>
2024-11-18 21:32:38 +0530

    MongoDB connected

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from "express";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import dotenv from "dotenv";
+import connectDB from "./utils/db.js";
 dotenv.config({});
 
 const app = express();
@@ -25,6 +26,7 @@ app.use(cors(corsOptions));
 
 const PORT = process.env.PORT || 3000
 app.listen(PORT,() => {
+    connectDB();
     console.log(`App is listening on port ${PORT}`);
 })
 
